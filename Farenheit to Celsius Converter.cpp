9c6b298fcd50f88bf6d8418e67dafacca4755948
Syam Konala <syamkonala30@gmail.com>
2023-06-20 09:43:18 -0500

    Add files via upload

---
+++ Farenheit to Celsius Converter.cpp
@@ -0,0 +1,33 @@
+// Syam Konala
+// BMEN 1300-002
+// 11/29/2022
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double celsius(int);
+
+int main()
+{
+	cout << "\nTable of Fahrenheit temperatures 0 - 20\n"
+		<< "and their Celsius equivalents.\n\n"
+		<< "------------------------------\n"
+		<< "  Fahrenheit       Celsius\n"
+		<< "------------------------------\n";
+
+	for (int F = 0; F <= 20; F++)
+	{
+		cout << "      " << setw(2) << F;
+		cout << "          "
+			<< setw(3) << celsius(F) << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
+double celsius(int F)
+{
+
+	return (5.0 * (F - 32)) / 9;
+}
